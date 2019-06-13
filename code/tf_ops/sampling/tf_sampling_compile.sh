d53c0af40457fb6172efbaebc5c2b69937450c4d
Prashant Raina <prashantraina2005@gmail.com>
2019-06-13 19:22:20 -0400

    CUDA 10 compatibility

--- code/tf_ops/sampling/tf_sampling_compile.sh
+++ code/tf_ops/sampling/tf_sampling_compile.sh
@@ -3,4 +3,5 @@ CUDA_HOME=/opt/cuda
 TF_CFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))')
 TF_LFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')
 nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I$CUDA_HOME/include $TF_CFLAGS $TF_LFLAGS -O2
+g++ -std=c++11 -c tf_sampling.cpp -o tf_sampling.o -fPIC -I$CUDA_HOME/include $TF_CFLAGS $TF_LFLAGS -O2
+nvcc -std=c++11 tf_sampling.o tf_sampling_g.cu.o -o tf_sampling_so.so -shared -I$CUDA_HOME/include $TF_CFLAGS $TF_LFLAGS -O2
